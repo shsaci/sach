8ff90fa4e1a8290e1420689a9afd8343eb7aa2a1
Sach <shsaci@yahoo.com.au>
2016-08-18 18:17:53 +1200

    not significant

--- server.babel.js
+++ server.babel.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 import index from './routes/index'
 
+app.use(cors())
 app.use('/', express.static('public'))
 
 app.get('/', index.get)
